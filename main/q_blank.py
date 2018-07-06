d434fefd54bd31138dc96b2b6777f116fb413796
SakataMoriLab <user1@SakataMoriLabnoMacBook.local>
2018-07-06 19:32:59 +0900

    changed values returned

--- main/q_blank.py
+++ main/q_blank.py
@@ -10,16 +10,15 @@ import collections
 import re
 import random
 
-
 #ここがメインプログラム。filenameと、q_num(欲しい問題数)で、問題をq_num問だけ出力する。
 #Ansをどういう形式にすべきかわからなかったので（ページ下部に出力？それとも、ページ上で答えられるようにする？）、暫定的に無視しています。
 def q_blank(filename, q_num):
-	f = open(filename, 'r')
-	text = f.read()
-	f.close()
-	words = find_important_words(text)
-	q_sets = make_blanked_question_set(words, text)
-	print_questions(q_sets, 10) 
+    f = open(filename, 'r')
+    text = f.read()
+    f.close()
+    words = find_important_words(text)
+    q_list = make_blanked_question_set(words, text)
+    return q_list[:q_num]
 
 #文章を入力すると、重要単語と、その重要度(tf-idf)をdictk型で返す関数。
 #本来はドキュメントがいくつもあるべきなんだけど、とりあえず一個の文章だけでやってます。(tf-idfの意味なしてないですね、すみません笑)
@@ -27,7 +26,7 @@ def find_important_words(text):
     m = MeCab.Tagger()
     result = m.parse(text)
     tagged_text = result
-	# 複合語を抽出し、重要度を算出
+    # 複合語を抽出し、重要度を算出
     frequency = termextract.mecab.cmp_noun_dict(tagged_text)
     LR = termextract.core.score_lr(frequency,
          ignore_words=termextract.mecab.IGNORE_WORDS,
@@ -45,7 +44,7 @@ def find_important_words(text):
 #問題文はランダムでその単語を含む文章を持ってくる。各単語について、問題文一つを対応させたdictが出力
 def make_blanked_question_set(words_dict, text):
     sentences = re.split('。', text) 
-    q_dict = dict()
+    q_list = list()
     for word, value in words_dict.items():
         q_for_word = list()
         for sentence in sentences:
@@ -54,10 +53,11 @@ def make_blanked_question_set(words_dict, text):
                 q = re.sub(word, '_____', sentence)
                 q_for_word.append(q)
         q_random = q_for_word[random.randrange(len(q_for_word))]
-        q_dict.update({q_random:word})   
-    return q_dict 
+        q_list.append({'question':q_random, 'answer':word})   
+    return q_list
 
 #問題文をq_num個分だけプリントする(多分ここは修正が必要)
+#今は使ってないプログラム
 def print_questions(q_dict, q_num):
     print("以下の空欄を埋めよ。\n")
     i = 0
@@ -69,6 +69,7 @@ def print_questions(q_dict, q_num):
 
 #プログラムが動くか確認する。sample.txtから問題を10問作る。
 if __name__ == "__main__":
-    q_blank('uploads/sample.txt', 10)
+    q_0 = q_blank('uploads/sample.txt', 10)
+    print(q_0)
 
 
