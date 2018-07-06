34700fa3265becc10713227eb44bbaacd81441b2
SakataMoriLab <user1@SakataMoriLabnoMacBook.local>
2018-07-06 17:21:17 +0900

    added comments to q_blank.py

--- main/q_blank.py
+++ main/q_blank.py
@@ -11,6 +11,8 @@ import re
 import random
 
 
+#ここがメインプログラム。filenameと、q_num(欲しい問題数)で、問題をq_num問だけ出力する。
+#Ansをどういう形式にすべきかわからなかったので（ページ下部に出力？それとも、ページ上で答えられるようにする？）、暫定的に無視しています。
 def q_blank(filename, q_num):
 	f = open(filename, 'r')
 	text = f.read()
@@ -19,7 +21,8 @@ def q_blank(filename, q_num):
 	q_sets = make_blanked_question_set(words, text)
 	print_questions(q_sets, 10) 
 
-#
+#文章を入力すると、重要単語と、その重要度(tf-idf)をdictk型で返す関数。
+#本来はドキュメントがいくつもあるべきなんだけど、とりあえず一個の文章だけでやってます。(tf-idfの意味なしてないですね、すみません笑)
 def find_important_words(text):
     m = MeCab.Tagger()
     result = m.parse(text)
@@ -30,7 +33,7 @@ def find_important_words(text):
          ignore_words=termextract.mecab.IGNORE_WORDS,
          lr_mode=1, average_rate=1)   
     term_imp = termextract.core.term_importance(frequency, LR)
-    # 重要度が高い順に並べ替えて出
+    # 重要度が高い順に並べ替えて出力
     words_dict = dict()
     data_collection = collections.Counter(term_imp)
     for cmp_noun, value in data_collection.most_common():
@@ -38,6 +41,8 @@ def find_important_words(text):
         words_dict.update({word:value})
     return words_dict
 
+#文章から単語を検索して、単語部分を＿＿＿に変更した問題文を作る関数。
+#問題文はランダムでその単語を含む文章を持ってくる。各単語について、問題文一つを対応させたdictが出力
 def make_blanked_question_set(words_dict, text):
     sentences = re.split('。', text) 
     q_dict = dict()
@@ -52,15 +57,17 @@ def make_blanked_question_set(words_dict, text):
         q_dict.update({q_random:word})   
     return q_dict 
 
+#問題文をq_num個分だけプリントする(多分ここは修正が必要)
 def print_questions(q_dict, q_num):
-    print("以下の空欄を埋めよ。")
+    print("以下の空欄を埋めよ。\n")
     i = 0
     for q,a in q_dict.items():
         i = i + 1
         print("第" + str(i) + "問")
-        print(q)
+        print(q + '\n')
         if i == q_num: break
 
+#プログラムが動くか確認する。sample.txtから問題を10問作る。
 if __name__ == "__main__":
     q_blank('uploads/sample.txt', 10)
 
